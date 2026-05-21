71a7941a546ccfbe0cfae95914b03e43287dd758
Thibault Le Goff <thib.legoff@gmail.com>
2026-05-21 13:24:27 +0200

    refactor(hypr): migrate config to lua

---
+++ dot_config/hypr/mocha.lua
@@ -0,0 +1,83 @@
+local C = {}
+
+C.rosewater = "rgb(f5e0dc)"
+C.rosewaterAlpha = "f5e0dc"
+
+C.flamingo = "rgb(f2cdcd)"
+C.flamingoAlpha = "f2cdcd"
+
+C.pink = "rgb(f5c2e7)"
+C.pinkAlpha = "f5c2e7"
+
+C.mauve = "rgb(cba6f7)"
+C.mauveAlpha = "cba6f7"
+
+C.red = "rgb(f38ba8)"
+C.redAlpha = "f38ba8"
+
+C.maroon = "rgb(eba0ac)"
+C.maroonAlpha = "eba0ac"
+
+C.peach = "rgb(fab387)"
+C.peachAlpha = "fab387"
+
+C.yellow = "rgb(f9e2af)"
+C.yellowAlpha = "f9e2af"
+
+C.green = "rgb(a6e3a1)"
+C.greenAlpha = "a6e3a1"
+
+C.teal = "rgb(94e2d5)"
+C.tealAlpha = "94e2d5"
+
+C.sky = "rgb(89dceb)"
+C.skyAlpha = "89dceb"
+
+C.sapphire = "rgb(74c7ec)"
+C.sapphireAlpha = "74c7ec"
+
+C.blue = "rgb(89b4fa)"
+C.blueAlpha = "89b4fa"
+
+C.lavender = "rgb(b4befe)"
+C.lavenderAlpha = "b4befe"
+
+C.text = "rgb(cdd6f4)"
+C.textAlpha = "cdd6f4"
+
+C.subtext1 = "rgb(bac2de)"
+C.subtext1Alpha = "bac2de"
+
+C.subtext0 = "rgb(a6adc8)"
+C.subtext0Alpha = "a6adc8"
+
+C.overlay2 = "rgb(9399b2)"
+C.overlay2Alpha = "9399b2"
+
+C.overlay1 = "rgb(7f849c)"
+C.overlay1Alpha = "7f849c"
+
+C.overlay0 = "rgb(6c7086)"
+C.overlay0Alpha = "6c7086"
+
+C.surface2 = "rgb(585b70)"
+C.surface2Alpha = "585b70"
+
+C.surface1 = "rgb(45475a)"
+C.surface1Alpha = "45475a"
+
+C.surface0 = "rgb(313244)"
+C.surface0Alpha = "313244"
+
+C.base = "rgb(1e1e2e)"
+C.baseAlpha = "1e1e2e"
+
+C.mantle = "rgb(181825)"
+C.mantleAlpha = "181825"
+
+C.crust = "rgb(11111b)"
+C.crustAlpha = "11111b"
+
+C.transparent = "rgba(00000000)"
+
+return C
